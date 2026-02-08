6d6eb7b10baabb8c091cf313961bd6f828e08752
Somalim-10 <mohmed43001@gmail.com>
2026-02-08 22:22:57 +0100

    added folders and interfaces, testet event repository

---
+++ backend/SomaliskDanskForening/SomaliskDanskForening-Lib/Interfaces/IEventRepo.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using SomaliskDanskForening_Lib.Models;
+
+namespace SomaliskDanskForening_Lib.Interfaces
+{
+    public interface IEventRepo
+    {
+        Event? Add(Event evt);
+        Event? GetById(int id);
+       
+        List<Event> GetAll();
+        Event? Update(Event evt);
+        Event? Delete(int id);
+    }
+}
